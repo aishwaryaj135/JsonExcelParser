9bf0299ba71cf5fa25648feb8cb01272490c5e2a
Aishwarya <aishwarya@corestrat.ai>
2025-04-11 17:32:13 +0100

    Remove redundant code

--- src/main/java/com/utt/parser/project/App.java
+++ src/main/java/com/utt/parser/project/App.java
@@ -7,7 +7,7 @@ public class App {
 		try {
 			// If the user wants to check what is been logged into the file on the screen
 			Logger.LOGTOMONITOR = false;
-			Logger.getInstance().log("Started the parser program");
+			logAndPrint("Started the parser program");
 			Scanner scIn = new Scanner(System.in);
 			Scanner scData = new Scanner(System.in);
 
@@ -15,7 +15,7 @@ public class App {
 			boolean flag = true;
 			Model model = new Model();
 			while (ch1 != 3 && flag == true) {
-				System.out.println(
+				logAndPrint(
 						"Enter 1 to parse the Array of Json Object Data to excel and mail the excel file \nEnter 2 to convert excel data to Json \nEnter 3 to exit");
 				if (scIn.hasNextInt()) {
 					ch1 = scIn.nextInt();
@@ -25,15 +25,11 @@ public class App {
 				}
 				switch (ch1) {
 				case 1: {
-					System.out.println(
-							"Entered 1 to parse the Array of Json Object Data to excel and mail the excel file");
-					Logger.getInstance()
-							.log("Entered 1 to parse the Array of Json Object Data to excel and mail the excel file");
+					logAndPrint("Entered 1 to parse the Array of Json Object Data to excel and mail the excel file");
 					flag = true;
 					int ch2 = 0;
 					while (ch2 != 5 && flag == true) {
-						System.out.println(
-								"Enter 1 to parse the Json Data from text file \nEnter 2 parse Json data from pdf \nEnter 3 parse Json data from Api \nEnter 4 to exit \nEnter 5 to go back to previous option");
+						logAndPrint("Enter 1 to parse the Json Data from text file \nEnter 2 parse Json data from pdf \nEnter 3 parse Json data from Api \nEnter 4 to exit \nEnter 5 to go back to previous option");
 						if (scIn.hasNextInt()) {
 							ch2 = scIn.nextInt();
 						} else {
@@ -42,60 +38,53 @@ public class App {
 						}
 						switch (ch2) {
 						case 1: {
-							System.out.println("Entered 1 to parse the Json Data from text file");
-							Logger.getInstance().log("Entered 1 to parse the Json data from text file");
-							System.out.println("Enter email id");
+							logAndPrint("Entered 1 to parse the Json data from text file");
+							logAndPrint("Enter email id");
 							String emailTo = scData.nextLine();
 							model.readDataFromTextFile();
 							model.sendEmail(emailTo);
 							break;
 						}
 						case 2: {
-							System.out.println("Entered 2 to parse the Array of Json Object Data from pdf");
-							Logger.getInstance().log("Entered 2 to parse the Array of Json Object Data from pdf");
+							logAndPrint("Entered 2 to parse the Array of Json Object Data from pdf");
 							model.readDataFromPdfFile();
 							break;
 						}
 						case 3: {
-							System.out.println("Entered 3 to parse the Array of Json Object Data from Api");
-							Logger.getInstance().log("Entered 3 to parse the Array of Json Object Data from Api");
+							logAndPrint("Entered 3 to parse the Array of Json Object Data from Api");
 
 							break;
 						}
 						case 4: {
-							System.out.println("Entered exit case");
-							Logger.getInstance().log("Entered exit case");
+							logAndPrint("Entered exit case");
 
 							flag = false;
 							break;
 						}
 						case 5: {
-							System.out.println("Entered 5 to go back");
-							Logger.getInstance().log("Entered 5 to go back");
+							logAndPrint("Entered 5 to go back");
 
 							break;
 						}
 						default:
-							System.out.println("Please enter the number from the option only");
+							logAndPrint("Please enter the number from the option only");
 							break;
 						}
 					}
 					break;
 				}
 				case 2: {
-					System.out.println("Entered 2 to convert excel data to Json");
-					Logger.getInstance().log("Entered 2 to convert excel data to Json");
+					logAndPrint("Entered 2 to convert excel data to Json");
 					model.excelToJson();
 					break;
 				}
 				case 3: {
-					System.out.println("Entered exit case");
-					Logger.getInstance().log("Entered exit case");
+					logAndPrint("Entered exit case");
 					flag = false;
 					break;
 				}
 				default:
-					System.out.println("Please enter the number from the option only");
+					logAndPrint("Please enter the number from the option only");
 					break;
 
 				}
@@ -106,4 +95,8 @@ public class App {
 			Logger.getInstance().log(t.getMessage());
 		}
 	}
+	private static void logAndPrint(String message) {
+        System.out.println(message);
+        Logger.getInstance().log(message);
+    }
 }
